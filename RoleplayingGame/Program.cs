443672489f03a1d83f860608b57fdd226cf5c396
kenenthLovgren <kenneth.l.thomsen@gmail.com>
2021-10-27 10:29:22 +0200

    Add project files.

---
+++ RoleplayingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoleplayingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
