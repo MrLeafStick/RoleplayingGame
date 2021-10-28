63b6eec580bf3fcf49aeb86d06c96addf1539864
kenenthLovgren <kenneth.l.thomsen@gmail.com>
2021-10-28 10:11:19 +0200

    Updated Character class with comments and virtual properties

--- RoleplayingGame/Character.cs
+++ RoleplayingGame/Character.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RoleplayingGame
+
 {
     public class Character
     {
@@ -37,12 +38,12 @@ namespace RoleplayingGame
         #region Properties
         public string Name
         {
-            get { return _name;  }
+            get { return _name; }
         }
 
         public bool IsDead
         {
-            get { return (_hitPoints <= 0);  }
+            get { return (_hitPoints <= 0); }
         }
         #endregion
 
@@ -75,7 +76,7 @@ namespace RoleplayingGame
             return 0;
         }
 
-        public int ReciveDamageModifier(int reciveDamage)
+        public int ReceiveDamageModifier(int receiveDamage)
         {
             return 0;
         }
@@ -83,6 +84,45 @@ namespace RoleplayingGame
         #endregion
 
         #region Virtual Properties and Methods
+        /// <summary>
+        /// Return the change of the damage dealt being modifed, 
+        /// unless overrides in a derived class, a charactor has 
+        /// 0% chance of having the damage dealt modified.
+        /// </summary>
+        protected virtual int DealDamageModifyChange
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Return the chance of the damage received bbeing modified.
+        /// Unless overrieded in a dirived class, a Character has 
+        /// 0% chance of having the damage received modified.
+        /// </summary>
+        protected virtual int ReceiveDamageModyChance
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Return the modifed dealt damage.
+        /// Unless overrisded in a dirived class, the modified dealt
+        /// damage is the same as the original dealt damage.
+        /// </summary
+        protected virtual int CalculateModifedDamage(int dealtDamage)
+        {
+            return dealtDamage;
+        }
+
+        /// <summary>
+        /// Return the modifed reveived damage.
+        /// unless orverried in a derved class the modifed received
+        /// damage is the sae as the original received damage.
+        /// </summary>
+        protected virtual int CalculateModifedReceivedDamage(int receivedDamage)
+        {
+            return receivedDamage;
+        }
 
         #endregion
     }
