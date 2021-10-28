b46b2cf186b41302b0848926ac20f8030d51cb36
kenenthLovgren <kenneth.l.thomsen@gmail.com>
2021-10-28 10:55:19 +0200

    Init commit,

--- RoleplayingGame/Character.cs
+++ RoleplayingGame/Character.cs
@@ -15,8 +15,6 @@ namespace RoleplayingGame
         protected int _maxHitPoints;
         protected int _minDamage;
         protected int _maxDamage;
-        protected int _GITDOOM;
-        protected int _DOOM_OF_DOOMS;
         #endregion
 
         #region Constructor
