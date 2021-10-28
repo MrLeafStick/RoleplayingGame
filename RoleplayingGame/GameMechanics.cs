4c5db7c0e85261db7e902ff5644aab9ae176d636
kenenthLovgren <kenneth.l.thomsen@gmail.com>
2021-10-28 09:00:04 +0200

    Ready To start on H2

--- RoleplayingGame/GameMechanics.cs
+++ RoleplayingGame/GameMechanics.cs
@@ -10,27 +10,6 @@ namespace RoleplayingGame
     {
         public void GameLogic()
         {
-            var swordA = new Sword("Slayer OF DOOOOOM", 20, 50);
-            var swordB = new Sword("Tiny Stick", 1, 5);
-
-
-            var warriorA = new Warrior("Ragnar", 200, swordB);
-            var warriorB = new Warrior("Sighurd", 240, swordA);
-
-            Console.WriteLine("Just after creation:");
-            Console.WriteLine(warriorA.GetInfo());
-            Console.WriteLine(warriorB.GetInfo());
-            Console.WriteLine();
-
-            int damageFromA = warriorA.DealDamage();
-            int damageFromB = warriorB.DealDamage();
-            warriorA.ReceiveDamage(damageFromB);
-            warriorB.ReceiveDamage(damageFromA);
-
-            Console.WriteLine("After damage:");
-            Console.WriteLine(warriorA.GetInfo());
-            Console.WriteLine(warriorB.GetInfo());
-            Console.WriteLine();
 
 
         }
