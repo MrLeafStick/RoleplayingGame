3405f95f9b962a54c446c5c00cad75866bedafb4
Victor Krogh Hansen <kingodk@outlook.dk>
2021-10-29 08:55:03 +0200

    Added Characters folder - new characters classes; Priest and Wizard

---
+++ RoleplayingGame/Characters/Wizard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleplayingGame
+{
+    public class Wizard : Character
+    {
+        public Wizard(string name, int hitPoints, int minDamage, int maxDamage) 
+            : base(name, hitPoints, minDamage, maxDamage)
+        {
+        }
+
+        protected override int DealDamageModifyChance
+        {
+            get { return 80; }
+        }
+
+        protected override int CalculateModifedDamage(int dealtDamage)
+        {
+            return (int)Math.Floor(dealtDamage * 2.5);
+        }
+
+        protected override int ReceiveDamageModifyChance
+        {
+            get { return 30; }
+        }
+
+        protected override int CalculateModifedReceivedDamage(int receivedDamage)
+        {
+            return (int)Math.Floor(receivedDamage / 2.5);
+        }
+    }
+}
