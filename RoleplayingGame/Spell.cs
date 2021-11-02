173196cbd05b8d9f3ead95bbdd91dea88611cbf0
Your Name <you@example.com>
2021-11-02 13:37:57 +0100

    Added Spell og SpellType classes

---
+++ RoleplayingGame/Spell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RolePlayingAbilityPrototyping.Spells
+{
+    public class Spell
+    {
+        public static IEnumerable<SpellType> SpellTypeList
+        {
+            get { return Enum.GetValues(typeof(SpellType)).Cast<SpellType>();  }
+        }
+    }
+}
---
+++ RoleplayingGame/SpellType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RolePlayingAbilityPrototyping.Spells
+{
+    public enum SpellType
+    {
+        IceStorm,
+        FireBall,
+        AcidRain,
+        ThunderBolt,
+        LavaBall,
+        Armadeddon
+    }
+}
