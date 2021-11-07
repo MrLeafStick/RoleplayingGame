a41da8158e37cea20f43b040c77fc23387505204
Your Name <you@example.com>
2021-11-07 14:29:27 +0100

    Added entity combinations and fixed Entity.ToString()

--- RPG-Sandbox-Enum-Classes-V1/Program.cs
+++ RPG-Sandbox-Enum-Classes-V1/Program.cs
@@ -41,46 +41,20 @@ namespace RPG_Sandbox_Enum_Classes_V1
         private EntityType(string type) { Name = type; }
         public string Name { get; }
 
-        public static EntityType Ghost { get { return new EntityType("Ghost"); } }
-        public static EntityType Were { get { return new EntityType("Were"); } }
-        public static EntityType Clockwork { get { return new EntityType("Clockwork"); } }
-        public static EntityType Undead { get { return new EntityType("Undead"); } }
-        public static EntityType Zombie { get { return new EntityType("Zombie"); } }
-        public static EntityType Skeleton { get { return new EntityType("Skeleton"); } }
-        public static EntityType Magical { get { return new EntityType("Magical"); } }
-        public static EntityType Living { get { return new EntityType("Living"); } }
+        public static EntityType Ghost      { get { return new EntityType("Ghost"); } }
+        public static EntityType Were       { get { return new EntityType("Were"); } }
+        public static EntityType Clockwork  { get { return new EntityType("Clockwork"); } }
+        public static EntityType Undead     { get { return new EntityType("Undead"); } }
+        public static EntityType Zombie     { get { return new EntityType("Zombie"); } }
+        public static EntityType Skeleton   { get { return new EntityType("Skeleton"); } }
+        public static EntityType Magical    { get { return new EntityType("Magical"); } }
+        public static EntityType Living     { get { return new EntityType("Living"); } }
 
         public static List<EntityType> List()
         {
             return new List<EntityType> { Ghost, Were, Clockwork, Undead, Zombie, Skeleton, Magical, Living };
         }
     }
-
-    //Peasant,
-    //Miller,
-    //Shopkeeper,
-    //Innkeeper,
-    //Sailor,
-    //Marauder,
-    //Torturer,
-    //King,
-    //Merchant,
-    //Blacksmith,
-    //Thief,
-    //Robber,
-    //Wizard,
-    //Warrior,
-    //Knight,
-    //Archer,
-    //Enchanterer,
-    //Mage,
-    //Necromancer,
-    //Alchemist,
-    //Warlock,
-    //Priest,
-    //Monk,
-    //Beast,
-
     public class EntityOccupation : IEntityOccupation
     {
         private EntityOccupation(string occupation) { Name = occupation; }
@@ -89,50 +63,26 @@ namespace RPG_Sandbox_Enum_Classes_V1
         public int SkillLevel { get; private set; }
         public void LevelUp() { SkillLevel++; }
 
-        public static EntityOccupation Peasant   { get { return new EntityOccupation("Peasant"); } }
-        public static EntityOccupation Blacksmith   { get { return new EntityOccupation("Blacksmith"); } }
-        public static EntityOccupation InnKeeper   { get { return new EntityOccupation("InnKeeper"); } }
-        public static EntityOccupation Thief        { get { return new EntityOccupation("Thief"); } }
-        public static EntityOccupation Alchemist    { get { return new EntityOccupation("Alchemist"); } }
-        public static EntityOccupation Wizard       { get { return new EntityOccupation("Wizard"); } }
-        public static EntityOccupation Torturer       { get { return new EntityOccupation("Torturer"); } }
-        public static EntityOccupation Warlock       { get { return new EntityOccupation("Warlock"); } }
-        public static EntityOccupation Necromancer       { get { return new EntityOccupation("Necromancer"); } }
+        public static EntityOccupation Peasant      { get { return new EntityOccupation("a Peasant"); } }
+        public static EntityOccupation Blacksmith   { get { return new EntityOccupation("a Blacksmith"); } }
+        public static EntityOccupation InnKeeper    { get { return new EntityOccupation("an InnKeeper"); } }
+        public static EntityOccupation Thief        { get { return new EntityOccupation("a Thief"); } }
+        public static EntityOccupation Robber        { get { return new EntityOccupation("a Robber"); } }
+        public static EntityOccupation Alchemist    { get { return new EntityOccupation("an Alchemist"); } }
+        public static EntityOccupation Wizard       { get { return new EntityOccupation("a Wizard"); } }
+        public static EntityOccupation Torturer     { get { return new EntityOccupation("a Torturer"); } }
+        public static EntityOccupation Warlock      { get { return new EntityOccupation("a Warlock"); } }
+        public static EntityOccupation Necromancer  { get { return new EntityOccupation("a Necromancer"); } }
+        public static EntityOccupation Warrior      { get { return new EntityOccupation("a Warrior"); } }
+        public static EntityOccupation Archer      { get { return new EntityOccupation("an Archer"); } }
+        public static EntityOccupation Priest      { get { return new EntityOccupation("a Priest"); } }
 
         public static List<EntityOccupation> List()
         {
-            return new List<EntityOccupation> { Peasant, Blacksmith, InnKeeper, Thief, Alchemist, Wizard, Torturer, Warlock, Necromancer };
+            return new List<EntityOccupation> { Peasant, Blacksmith, InnKeeper, Thief, Robber, Alchemist, Wizard, Torturer, Warlock, Necromancer, Warrior, Archer, Priest };
         }
 
     }
-
-    //Human,
-    //Elf,
-    //Sprite,
-    //Ork,
-    //Troll,
-    //Dwarf,
-    //Giant,
-    //Hobgoblin,
-    //Gargoyle,
-    //Griffin,
-    //Centaur,
-    //Minotaur,
-    //Dragon,
-    //Unicorn,
-    //Golem,
-    //Wolf,
-    //Goat,
-    //Bear,
-    //Snake,
-    //Owl,
-    //Lion,
-    //Fox,
-    //Eagle,
-    //Orca,
-    //Squid,
-    //Whale,
-    //Sealion
 
     public class EntitySpecies
     {
@@ -151,17 +101,18 @@ namespace RPG_Sandbox_Enum_Classes_V1
         public static EntitySpecies Ork         { get { return new EntitySpecies("Ork", 700, 1000); } }
         public static EntitySpecies Elf         { get { return new EntitySpecies("Elf", 400, 1500); } }
         public static EntitySpecies Dwarf       { get { return new EntitySpecies("Dwarf", 800, 700); } }
+        public static EntitySpecies Giant       { get { return new EntitySpecies("Giant", 1800, 1700); } }
         public static EntitySpecies Hobgoblin   { get { return new EntitySpecies("Hobgoblin", 200, 100); } }
         public static EntitySpecies Griffin     { get { return new EntitySpecies("Griffin", 2000, 2000); } }
         public static EntitySpecies Minotaur    { get { return new EntitySpecies("Minotaur", 1500, 2500); } }
-        public static EntitySpecies Wolf    { get { return new EntitySpecies("Wolf", 1500, 2500); } }
-        public static EntitySpecies Goat    { get { return new EntitySpecies("Goat", 50, 25); } }
-        public static EntitySpecies Snake    { get { return new EntitySpecies("Snake", 150, 250); } }
-        public static EntitySpecies Bear    { get { return new EntitySpecies("Bear", 500, 500); } }
+        public static EntitySpecies Wolf        { get { return new EntitySpecies("Wolf", 1500, 2500); } }
+        public static EntitySpecies Goat        { get { return new EntitySpecies("Goat", 50, 25); } }
+        public static EntitySpecies Snake       { get { return new EntitySpecies("Snake", 150, 250); } }
+        public static EntitySpecies Bear        { get { return new EntitySpecies("Bear", 500, 500); } }
 
         public static List<EntitySpecies> List()
         {
-            return new List<EntitySpecies> { Human, Ork, Elf, Dwarf, Hobgoblin, Griffin, Minotaur, Wolf, Goat, Snake, Bear };
+            return new List<EntitySpecies> { Human, Ork, Elf, Dwarf, Giant, Hobgoblin, Griffin, Minotaur, Wolf, Goat, Snake, Bear };
         }
     }
 
@@ -183,7 +134,7 @@ namespace RPG_Sandbox_Enum_Classes_V1
 
         public override string ToString()
         {
-            var entityString = $"{Name} the {Type.Name} {Species.Name} is a {Occupation.Name}";
+            var entityString = $"{Name} the {(Type.Name == "Living" ? "\b" : Type.Name)}{(Type.Name == "Were" ? "-" : " ")}{Species.Name} is {Occupation.Name}";
 
             var abilityString = $"{Name} has the following abilities:\n";
 
